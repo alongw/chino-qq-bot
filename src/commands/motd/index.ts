0e95b11c6f7233869eb7b2393171ceda8a088848
alongw <alongw@alongw.cn>
2024-03-01 16:39:52 +0800

    feat: motd command deep time

--- src/commands/motd/index.ts
+++ src/commands/motd/index.ts
@@ -1,6 +1,10 @@
 import { useMotd } from './hooks/useMotd'
 
+import dayjs from 'dayjs'
+
 import type { Event } from '@/types/user'
+
+let exp: number | undefined
 
 export default async (args: string[], event: Event) => {
     if (!args[0]) {
@@ -9,6 +13,15 @@ export default async (args: string[], event: Event) => {
             content: '命令执行失败：Not enough parameters'
         })
     }
+
+    if (exp && dayjs().valueOf() < exp) {
+        return event.reply({
+            msg_type: 0,
+            content: '命令执行失败：Too many requests, please try again later.'
+        })
+    }
+
+    exp = dayjs().add(15, 's').valueOf()
 
     const result = await useMotd(args[0], args[1] ? +args[1] : undefined)
     if (!result) {
@@ -23,6 +36,8 @@ export default async (args: string[], event: Event) => {
             result.status
         )}\n人数：${JSON.stringify(result.players_online)}/${JSON.stringify(
             result.players_max
-        )}\n版本：${JSON.stringify(result.version || '未知')}`
+        )}\n版本：${JSON.stringify(
+            result.version || '未知'
+        )}\n\n以上信息仅供参考，由服务器自身提供，与本机器人无关`
     })
 }
