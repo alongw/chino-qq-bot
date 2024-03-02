93dbcca84c67cc5b629d88d42b991ff1d7257494
alongw <alongw@alongw.cn>
2024-03-02 13:09:39 +0800

    feat: shit qq

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -7,6 +7,7 @@ import Motd from './motd'
 
 commands.set('/help', Help)
 // 煞笔 QQ 你妈的短横线不是英文字符是吧，中文不行英文限制这么多 还是用 Mirai 好了
+commands.set('/check-in', CheckIn)
 commands.set('/checkIn', CheckIn)
 commands.set('/me', Me)
 commands.set('/motd', Motd)
