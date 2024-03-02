3ab34740faa93638ea35bc0797c9a79db97a91ab
alongw <alongw@alongw.cn>
2024-03-02 18:29:46 +0800

    feat: jrrp command

--- src/commands/jrrp/index.ts
+++ src/commands/jrrp/index.ts
@@ -1,14 +1,32 @@
+import dayjs from 'dayjs'
+
 import { useUserCharacter, getMsg } from './hooks/useUserCharacter'
 
 import type { Event } from '@/types/user'
 
+const value = new Map<
+    string,
+    {
+        luck: number
+        exp: number
+    }
+>()
+
 export default (args: string[], event: Event) => {
     const { getCharacter } = useUserCharacter(event.user.id)
 
-    const luckValue = getCharacter()
+    let luckValue = value.get(event.user.id)
+
+    if (!luckValue || dayjs().get('date') !== luckValue.exp) {
+        luckValue = {
+            luck: getCharacter(),
+            exp: dayjs().get('date')
+        }
+        value.set(event.user.id, luckValue)
+    }
 
     event.reply({
-        content: `你今天的人品是：${luckValue}，${getMsg(luckValue)}`,
+        content: `你今天的人品是：${luckValue.luck}，${getMsg(luckValue.luck)}`,
         msg_type: 0
     })
 }
