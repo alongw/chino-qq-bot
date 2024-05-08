9d20d87bb707f9cb8094a3d7fad8e7eecf9b04a8
alongw <alongw@alongw.cn>
2024-05-08 23:00:02 +0800

    feat(web): install mdui

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineOptions({
-  name: 'AppComponent'
+    name: 'AppComponent'
 })
 </script>
 
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,3 +1,5 @@
+import '@/assets/style/base.less'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
