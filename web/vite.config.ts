4c1791a139019f479c74021cdb548fcc211b2f99
alongw <alongw@alongw.cn>
2024-05-08 22:58:36 +0800

    feat(web): install mdui

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,12 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    // 所有以 mdui- 开头的标签名都是 mdui 组件
+                    isCustomElement: (tag) => tag.startsWith('mdui-')
+                }
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
