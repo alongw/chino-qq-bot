c76d92a9fa0f704951a5deb0a9c704cf7165fba8
alongw <alongw@alongw.cn>
2024-05-08 23:09:49 +0800

    feat(web): install mdui

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,4 +4,11 @@ defineOptions({
 })
 </script>
 
-<template>app</template>
+<template>
+    <mdui-top-app-bar style="position: relative">
+        <mdui-button-icon icon="menu"></mdui-button-icon>
+        <mdui-top-app-bar-title>Title</mdui-top-app-bar-title>
+        <div style="flex-grow: 1"></div>
+        <mdui-button-icon icon="more_vert"></mdui-button-icon>
+    </mdui-top-app-bar>
+</template>
