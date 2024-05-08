14263d0eab2f796606f5a8b2aa05d968b3e22ac3
alongw <alongw@alongw.cn>
2024-05-08 22:57:15 +0800

    feat(web): install mdui

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'mdui/mdui.css'
+import 'mdui'
+
 const app = createApp(App)
 
 app.use(createPinia())
